317d68edf9c83de40cc1919fcfdb2000165d088a
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-10-21 18:55:04 +0300

    comments

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,21 @@
+// Імпортуючи useState з бібліотеки React для управління станом у функціональному компоненті
 import { useState } from 'react';
+
+// Імпортуючи стилізовані компоненти з вказаних файлів
 import { MainContainer } from "./App.styled";
 import { Section } from "../Section/Section";
 import { FeedbackOptions } from "../FeedbackOptions/FeedbackOptions";
 import { Statistics } from "../Statistics/Statistics";
 import { Notification } from "../Notification/Notification";
 
+// Визначаємо головний компонент App
 export const App = () => {
+  // Використання useState для управління станом для зберігання "добрих", "нейтральних" та "поганих" відгуків
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  // Функція для обробки натискань кнопок відгуку та оновлення відповідного стану
   const onLeaveFeedback = state => {
     switch (state) {
       case 'good':
@@ -29,18 +35,22 @@ export const App = () => {
     }
   };
 
+  // Функція для підрахунку загальної кількості відгуків
   const countTotalFeedback = () => {
     return good + neutral + bad;
   };
 
+  // Функція для підрахунку відсотка позитивних відгуків
   const countPositiveFeedbackPercentage = () => {
     const total = countTotalFeedback();
     return Math.round((good / total) * 100) || 0;
   };
 
-    const options = Object.keys({ good, neutral, bad } );
-    const totalFeedback = countTotalFeedback();
-    const totalPercentage = countPositiveFeedbackPercentage();
+  // Створення масиву опцій для відгуків ('good', 'neutral', 'bad')
+  const options = Object.keys({ good, neutral, bad });
+  // Розрахунок загальної кількості відгуків та відсотка позитивних відгуків
+  const totalFeedback = countTotalFeedback();
+  const totalPercentage = countPositiveFeedbackPercentage();
     
     return (
       <MainContainer>
